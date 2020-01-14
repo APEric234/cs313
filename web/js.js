d20845722d14518d0b4c56c68de7919f1afbdeac
Eric Allen <aperic234@gmail.com>
2020-01-14 16:54:47 -0700

    added fade out

--- web/js.js
+++ web/js.js
@@ -1,9 +1,13 @@
-$(function(){(".button").click(function(){
+$(function(){(".click").click(function(){
   $(this).innerHTML = "Clicked!";
 })
 });
 
-$(function(){("button2").click(function(){
+$(function(){(".color").click(function(){
   $('.a1').css("background-color", $('.colorInput').innerHtml)
+})
+});
+$(function(){(".visibility").click(function(){
+  $('.a3').fadeOut()
 })
 });
