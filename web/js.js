34f1f76dd25b3cdba9a77d780394ba6c99c72772
Eric Allen <aperic234@gmail.com>
2020-01-14 16:48:30 -0700

    fixed

--- web/js.js
+++ web/js.js
@@ -1,6 +1,4 @@
 $(function(){(".button").click(function(){
   $(this).innerHTML = "Clicked!";
-}
-function colorchange(){
-  document.getElementsByClassName("a1")[0].style.backgroundColor = document.getElementsByClassName("colorInput")[0].value
-}
+  })
+});
