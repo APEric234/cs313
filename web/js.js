b46723007fee4df693edad7027951f3b5b4ce7df
Eric Allen <aperic234@gmail.com>
2020-01-14 17:14:22 -0700

    test to set purple

--- web/js.js
+++ web/js.js
@@ -4,12 +4,13 @@ $(function(){
 });
 
   $(".color").click(function(){
-  $('.a1').css("background-color", $('.colorInput').html())
+  $('.a1').css("background-color", "purple")
 });
 
 
   $(".visibility").click(function(){
   $('.a3').fadeOut(2500)
+  $('.a3').fadeIn(2500)
 });
 
 });
