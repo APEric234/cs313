65446b71119697ad349f296609e6e4caf6b9d904
Eric Allen <aperic234@gmail.com>
2020-01-14 17:07:57 -0700

    fixed?

--- web/js.js
+++ web/js.js
@@ -1,14 +1,15 @@
-$(function(){(".click").click(function(){
+$(function(){
+  $(".click").click(function(){
   $(this).innerHTML = "Clicked!";
-})
 });
 
-$(function(){(".color").click(function(){
+  $(".color").click(function(){
   $('.a1').css("background-color", $('.colorInput').innerHtml)
-})
 });
 
-$(function(){(".visibility").click(function(){
+
+  $(".visibility").click(function(){
   $('.a3').fadeOut(2500)
-})
+});
+
 });
