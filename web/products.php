b65cb72f1ae5db6f6eddaac9c13509297a9e793f
Eric Allen <aperic234@gmail.com>
2020-01-25 21:52:30 -0700

    fixed

--- web/products.php
+++ web/products.php
@@ -3,15 +3,9 @@
 <body>
 
 <h1>Cart</h1>
-
-You have purchased <?php echo $_POST["green"];?> green paint <br>
-
-You have purchased <?php echo $_POST["blue"];?> blue paint <br>
-
-You have purchased <?php echo $_POST["red"];?> red paint <br>
-
-You have purchased <?php echo $_POST["yellow"];?> yellow paint <br>
-<h2>Cart </h2>
-
+<p class="yellow">You have purchased <?php echo $_POST["yellow"];?> yellow paint </p>
+<p class="green">You have purchased <?php echo $_POST["green"];?> green paint </p>
+<p class="red">You have purchased <?php echo $_POST["red"];?> red paint </p>
+<p class="blue">You have purchased <?php echo $_POST["blue"];?> blue paint </p>
 </body>
 </html>
