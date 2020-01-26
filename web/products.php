06fd559e30bc66ddf2af45a9fc8da9965caa6783
Eric Allen <aperic234@gmail.com>
2020-01-25 21:33:50 -0700

    for sure this time

--- web/products.php
+++ web/products.php
@@ -7,13 +7,14 @@ session_start();
 <body>
 
 <h1>Products</h1>
-
+=
 <?php
 $colors = array("red", "green", "blue", "yellow");
 
 foreach ($colors as $value) {
-  $class=$value
-  echo "$value <br><button class=\"$class\">Buy</button>";
+  $class=$value;
+  echo "$value <br>";
+  echo "<button class=\"$class\">Buy</button>";
 }
 ?>
 
