f77fbf519b09cf11c352b9450a3d478f30019c24
Eric Allen <aperic234@gmail.com>
2020-01-25 22:08:08 -0700

    fixed cart removal

---
+++ web/cart.js
@@ -0,0 +1,10 @@
+$(function(){
+  $(".remove").click(function(){
+    var theId = this.id;
+    var b= document.getElementsByClassName(theId);
+    b[0].style.display = "none";
+
+  
+});
+
+});
--- web/products.php
+++ web/products.php
@@ -1,12 +1,13 @@
 
 <html>
+<script src="cart.js"></script>
 <body>
 
 <h1>Cart</h1>
-<p class="yellow">You have purchased <?php echo $_POST["yellow"];?> yellow paint </p><button class="remove yellow">Remove the yellow paint from your cart?</button>
-<p class="green">You have purchased <?php echo $_POST["green"];?> green paint </p><button class="remove green">Remove the green paint from your cart?</button>
-<p class="red">You have purchased <?php echo $_POST["red"];?> red paint </p><button class="remove red">Remove the red paint from your cart?</button>
-<p class="blue">You have purchased <?php echo $_POST["blue"];?> blue paint </p> <button class="remove blue">Remove the blue paint from your cart?</button>
+<p class="yellow">You have purchased <?php echo $_POST["yellow"];?> yellow paint </p><button class="remove" id="yellow">Remove the yellow paint from your cart?</button>
+<p class="green">You have purchased <?php echo $_POST["green"];?> green paint </p><button class="remove" id="green">Remove the green paint from your cart?</button>
+<p class="red">You have purchased <?php echo $_POST["red"];?> red paint </p><button class="remove" id="red">Remove the red paint from your cart?</button>
+<p class="blue">You have purchased <?php echo $_POST["blue"];?> blue paint </p> <button class="remove" id=" blue">Remove the blue paint from your cart?</button>
 
 </body>
 </html>
