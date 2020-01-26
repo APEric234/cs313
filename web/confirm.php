344dc44a80e806e5aa69ae364a8f04d6393804e1
Eric Allen <aperic234@gmail.com>
2020-01-25 22:42:48 -0700

    fixed bad copy

--- web/confirm.php
+++ web/confirm.php
@@ -24,13 +24,6 @@ if $red > 0{
     <P> We will ship your order too <?php 
     $text=$_POST["address"];
     $text = preg_replace('/[^a-z0-9 ]+/','',$text);
-    echo $text;?>
-    
-    You are ordering Green Paint: <input type="number" name="red" value = "<?php echo $_POST["green"];?>" readonly><br>
-    You are ordering Blue Paint: <input type="number" name="red" value = "<?php echo $_POST["blue"];?>" readonly><br>
-    You are ordering Yellow Paint: <input type="number" name="red" value = "<?php echo $_POST["yellow"];?>" readonly><br>
-    Your shipping address is : <input type="text" name="address">
-    <p>Push submit to confirm your address</p>
-    <input type="submit">
+    echo $text;?></p>
 </body>
 </html>
--- web/products.php
+++ web/products.php
@@ -8,9 +8,9 @@
 <form action="shipping.php" method="post">
     You have Red Paint: <input type="number" name="red" value = "<?php echo $_POST["red"];?>" required><br>
     
-    You have Green Paint: <input type="number" name="red" value = "<?php echo $_POST["green"];?>" required><br>
-    You have Blue Paint: <input type="number" name="red" value = "<?php echo $_POST["blue"];?>" required><br>
-    You have Yellow Paint: <input type="number" name="red" value = "<?php echo $_POST["yellow"];?>" required><br>
+    You have Green Paint: <input type="number" name="green" value = "<?php echo $_POST["green"];?>" required><br>
+    You have Blue Paint: <input type="number" name="blue" value = "<?php echo $_POST["blue"];?>" required><br>
+    You have Yellow Paint: <input type="number" name="yellow" value = "<?php echo $_POST["yellow"];?>" required><br>
     <p>Push submit to confirm your cart</p>
     <input type="submit">
 </body>
