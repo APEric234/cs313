5f502695b93fe6faed2549d7f63eed6ea0ad912d
Eric Allen <aperic234@gmail.com>
2020-01-25 22:24:08 -0700

    confirm cart

--- web/products.php
+++ web/products.php
@@ -5,10 +5,12 @@
 <body>
 
 <h1>Cart</h1>
-<?php if $_POST["yellow"] > 0 {echo "<p class=\"yellow\">You have purchased $_POST["yellow"]; gallons of yellow paint </p><button class=\"remove\" id=\"yellow\">Remove the yellow paint from your cart?</button>
-<p class="green">You have purchased <?php echo $_POST["green"];?> green paint </p><button class="remove" id="green">Remove the green paint from your cart?</button>
-<p class="red">You have purchased <?php echo $_POST["red"];?> red paint </p><button class="remove" id="red">Remove the red paint from your cart?</button>
-<p class="blue">You have purchased <?php echo $_POST["blue"];?> blue paint </p> <button class="remove" id=" blue">Remove the blue paint from your cart?</button>
-<a href="products.html">Back to Paint Store</a>
+<form action="products.php" method="post">
+    You have Red Paint: <input type="number" name="red" value = "<?php echo $_POST["red"];?>" required><br>
+    
+    You have Red Paint: <input type="number" name="red" value = "<?php echo $_POST["green"];?>" required><br>
+    You have Red Paint: <input type="number" name="red" value = "<?php echo $_POST["blue"];?>" required><br>
+    You have Red Paint: <input type="number" name="red" value = "<?php echo $_POST["yellow"];?>" required><br>
+    <p>Push submit to confirm your cart</p>
 </body>
 </html>
