e3de042c108fd81be6d4c639d8c8aec9d59f86dd
Eric Allen <aperic234@gmail.com>
2020-01-25 21:17:34 -0700

    fixed products

---
+++ web/products.php
@@ -0,0 +1,25 @@
+<?php
+// Start the session
+session_start();
+?>
+<!DOCTYPE html> 
+<html>
+<body>
+
+<h1>Products</h1>
+
+<?php
+$_SESSION["favcolor"] = "green paint";
+$a = $_SESSION["favcolor"];
+$b=array($_SESSION["favcolor"],"yellow paint");
+$d = array("")
+foreach ($b as $c){
+  foreach($d as $price){
+    echo ("<p>$c costs $price <button class="$c">Buy</button></p>"); 
+  }
+
+}?>
+
+
+</body>
+</html>
