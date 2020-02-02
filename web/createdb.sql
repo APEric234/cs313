7670c505d0cc8d1ec95a853624a7b6f96d042ec7
Eric Allen <aperic234@gmail.com>
2020-02-01 22:35:56 -0700

    pushing up updated db

--- web/createdb.sql
+++ web/createdb.sql
@@ -6,11 +6,11 @@ CREATE TABLE users(
 create table characters(
  character_id  INT primary key
 , fname   varchar(20)
-,users_id int, agi  int,str  int,wis  int,intel  int,charisma  int,
+,users_id int, agil  int,stre  int,wisd  int,intel  int,grace  int,
  foreign key (users_id) references users(user_id));
  
 create table equipment(
- character_id  INT primary key
+ equip_id  INT primary key
 , fname   varchar(20)
-,users_id int, agi  int,str  int,wis  int,intel  int,charisma  int,
- foreign key (users_id) references users(user_id));
+,characters_id int, agiBuff  int,streBuff  int,wisBuff  int,intelBuff  int,graceBuff  int,
+ Special varchar(30), foreign key (characters_id) references characters(character_id));
