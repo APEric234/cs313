611f56d7838f5fb358fb5d0a9a02a65b78882213
Eric Allen <aperic234@gmail.com>
2020-02-08 22:04:42 -0700

    removed stmnt

--- web/characters.php
+++ web/characters.php
@@ -31,7 +31,7 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $db = get_db()
 $query = 'select fname from characters;'
-$stmnt = $db->prepare($query);
+#$stmnt = $db->prepare($query);
 #$names = $stmnt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <html>
