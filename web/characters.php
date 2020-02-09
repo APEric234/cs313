07eb0113943e8b5ea78615689aa88908e30b355c
Eric Allen <aperic234@gmail.com>
2020-02-08 22:38:34 -0700

    its late work please

--- web/characters.php
+++ web/characters.php
@@ -30,17 +30,19 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
 $db = get_db()
-$query = 'select fname from characters;';
-#$stmnt = $db->prepare($query);
-#$names = $stmnt->fetchAll(PDO::FETCH_ASSOC);
+$query = 'select fname from characters';
+$stmnt = $db->query($query);
+$stmnt -> execute();
+$names = $stmnt->fetchAll();
+
 ?>
 <html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="cart.js"></script>
 <body>
 <?php 
-#foreach ($names as $name){
-#  $namep=$name['fname'];
-#  echo("<p>The Epic <b>$namep</b> has entered the game </p>")
-#}
+foreach ($names as $name){
+  $namep=$name['fname'];
+  echo("<p>The Epic <b>$namep</b> has entered the game </p>");
+}
 ?>
