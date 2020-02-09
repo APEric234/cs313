3d375d57dad4cd7092ec7ea03a46098635bcdd9b
Eric Allen <aperic234@gmail.com>
2020-02-08 21:53:45 -0700

    added error handling and return statement

--- web/db.php
+++ web/db.php
@@ -1,6 +1,8 @@
 <?php 
 
 function get_DB(){
+  $db = null;
+  try{
 $dbUrl = getenv('DATABASE_URL');
 $dbOpts = parse_url($dbUrl);
 $dbHost = $dbOpts["host"];
@@ -10,6 +12,10 @@ $dbPassword = $dbOpts["pass"];
 $dbName = ltrim($dbOpts["path"],'/');
 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }catch(PDOException $ex){
+    echo "Error connecting to db. detalis: $ex"
+  }
+  return $db;
 }
 
 ?>
