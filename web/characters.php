dc4444e03b5b7baa21501e3adafaf96c4858f12a
Eric Allen <aperic234@gmail.com>
2020-02-08 21:15:29 -0700

    does it exist

--- web/characters.php
+++ web/characters.php
@@ -1,11 +1,4 @@
 
-<html>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="cart.js"></script>
-<body>
-
-<h1>Order Confirmation</h1>
-
 
 <html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -14,27 +7,21 @@
 
 <h1>Order Confirmation</h1>
 
+
+<html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="cart.js"></script>
+<body>
 <?php 
+$db_url = getenv('DATABSE_URL');
+echo "<h2> $db_url is </h2>"
 $conn_string = $db_connection = pg_connect(
   "host=localhost port=5432 dbname=dergo389khd0pd"
 );
-$conn = pg_pconnect($conn_string);
-if (!$conn) {
-    echo "An error occurred. here\n";
-    exit;
-}
-
-$result = pg_query($conn, "SELECT * FROM equipment");
-if (!$result) {
-    echo "An error occurred. 2 here\n";
-    exit;
-}
-
-$arr = pg_fetch_all($result);
-
-print_r($arr);
-
 ?>
+<h1>Order Confirmation</h1>
+
+
     <P> We will ship your order to <?php 
     echo htmlspecialchars($_POST["address"])
     ?></p>
