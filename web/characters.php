dcabe98a2f0e4584fcd00e5dc1b030f573950840
Eric Allen <aperic234@gmail.com>
2020-02-08 21:49:15 -0700

    please work

--- web/characters.php
+++ web/characters.php
@@ -7,29 +7,20 @@
 
 <h1>Order Confirmation</h1>
 
-
+<?php 
+require('dbConnect.php');
+$db = get_db()
+$query = 'select  fname from characters'
+$stmnt = $db->prepare($query);
+$names = $stmnt->fetchAll(PDO::FETCH_ASSOC);
+?>
 <html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="cart.js"></script>
 <body>
 <?php 
-
-
-$dbUrl = getenv('DATABASE_URL');
-$dbOpts = parse_url($dbUrl);
-$dbHost = $dbOpts["host"];
-$dbPort = $dbOpts["port"];
-$dbUser = $dbOpts["user"];
-$dbPassword = $dbOpts["pass"];
-$dbName = ltrim($dbOpts["path"],'/');
-$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
+foreach ($names as $name){
+  $namep=$name['fname'];
+  echo("<p>The Epic <b>$namep</b> has entered the game </p>")
+}
 ?>
-<h1>Order Confirmation</h1>
-</body>
-</html>
-
-</body>
-</html>
