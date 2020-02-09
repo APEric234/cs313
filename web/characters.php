847c80a6d98f1a166f9354acae0c48189e855ef5
Eric Allen <aperic234@gmail.com>
2020-02-08 21:55:25 -0700

    testing

--- web/characters.php
+++ web/characters.php
@@ -10,17 +10,17 @@
 <?php 
 require('db.php');
 $db = get_db()
-$query = 'select fname from characters'
-$stmnt = $db->prepare($query);
-$names = $stmnt->fetchAll(PDO::FETCH_ASSOC);
+#$query = 'select fname from characters'
+#$stmnt = $db->prepare($query);
+#$names = $stmnt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <html>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="cart.js"></script>
 <body>
 <?php 
-foreach ($names as $name){
-  $namep=$name['fname'];
-  echo("<p>The Epic <b>$namep</b> has entered the game </p>")
-}
+#foreach ($names as $name){
+#  $namep=$name['fname'];
+#  echo("<p>The Epic <b>$namep</b> has entered the game </p>")
+#}
 ?>
