fbb5a86d353ede1cfdaa0c6190d627a85de26cdb
Eric Allen <aperic234@gmail.com>
2020-02-08 21:17:04 -0700

    fix

--- web/characters.php
+++ web/characters.php
@@ -13,7 +13,7 @@
 <script src="cart.js"></script>
 <body>
 <?php 
-$db_url = getenv('DATABSE_URL');
+$db_url = getenv('DATABASE_URL');
 echo "<h2> $db_url is </h2>";
 $conn_string = $db_connection = pg_connect(
   "host=localhost port=5432 dbname=dergo389khd0pd"
