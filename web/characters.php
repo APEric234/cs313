479744ebf7c745abce19afc7a4054533a57c7c25
Eric Allen <aperic234@gmail.com>
2020-02-15 13:25:44 -0700

    making them one to a line

--- web/characters.php
+++ web/characters.php
@@ -22,7 +22,7 @@ $stmnt -> execute();
 $names = $stmnt->fetchAll();
 foreach ($names as $name){
   $namep=$name['fname'];
-  echo("<li class='hero'>The Epic <b>$namep</b> has entered the game </li><br>");
+  echo("<li class='hero'>The Epic <b>$namep</b> has entered the game </li><br/><br/>");
 }
 
 ?>
