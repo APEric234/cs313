095deca0ea3100508e9676e2d026769bd36abaa1
Eric Allen <aperic234@gmail.com>
2020-02-15 15:17:56 -0700

    added missing ()

--- web/success_player.php
+++ web/success_player.php
@@ -23,7 +23,6 @@ require_once('db.php')
       $stmnt -> execute();
       $id_duplicate = $stmnt->fetchAll(); 
     }
-
     $name=htmlspecialchars($_POST['name']);
     
     $email=htmlspecialchars($_POST['email']);
@@ -34,21 +33,21 @@ require_once('db.php')
       $query2 = "insert  into users (User_id,Fname,Email) Values($id,'$name','$email');";
       $stmnt = $db->query($query2);
       $stmnt -> execute();
-      echo "
+      echo("
         <html><body>
         <b> Your have been added as a user your id is $id please remember this so you can add a character later<b>
         <a href=\"characters.php\"> Click here to go and get a hero added for your user!</a>
 
         </body></html>
-      ";
+      ");
       }catch{
-        echo "
+        echo("
         <html><body>
         <b> Your have already added as a user please refresh the page<b>
         <a href=\"characters.php\"> Click here to go and get a hero added for your user!</a>
 
         </body></html>
-      ";
+      ");
       }
     }
 
