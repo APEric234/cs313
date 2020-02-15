b74270f1aeecc862554e4492754bf8a50f8e7bd8
Eric Allen <aperic234@gmail.com>
2020-02-14 21:01:22 -0700

    added missing ""

--- web/fail.php
+++ web/fail.php
@@ -1,5 +1,5 @@
 <html><body>
   <b> Your hero couldn't be added to the list of characters due to an error with the user_id you gave<b>
-  <a href=characters.php> Click here to go back and try again</a>
+  <a href="characters.php"> Click here to go back and try again</a>
   
   </body></html>
--- web/success.php
+++ web/success.php
@@ -1,5 +1,5 @@
 <html><body>
   <b> Your hero has been added to the list of characters succesfully!<b>
-  <a href=characters.php> Click here to go back and see him on the list of hero's!</a>
+  <a href="characters.php"> Click here to go back and see him on the list of hero's!</a>
   
   </body></html>
