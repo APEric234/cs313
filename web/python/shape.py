cdc15d5c4a1542c6ac21bed6fcbe633d829717f6
Eric Allen <aperic234@gmail.com>
2020-02-19 19:55:29 -0700

    new python classes

---
+++ web/python/shape.py
@@ -0,0 +1,25 @@
+
+
+class Shape:
+  def __init__(self):
+    self.name="Shape"
+  def get_area(self):
+    pass
+
+class Circle(Shape):
+  def __init__(self):
+    super().__init__()
+    self.name="Circle"
+    self.radius="0"
+  def get_area(self):
+    return 3.14*self.radius*self.radius
+
+
+class Rectangle (Shape):
+  def __init__(self):
+    super().__init__()
+    self.name="Rectangle"
+    self.length=0
+    self.width=0
+  def get_area(self):
+    return self.length*self.width
