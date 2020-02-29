af4cc3b17af618f200e4e59b5d1114c923c82dd4
Eric Allen <aperic234@gmail.com>
2020-02-29 16:54:53 -0700

    added additional files

--- web/python/gpa.py
+++ web/python/gpa.py
@@ -3,26 +3,28 @@
 class GPA():
 #create init and set default gpa to zero
   def __init__(self):
-    self.gpa=0
+    self.__gpa=0
     self.letters = {'A':4.0,'B':3.0,'C':2.0,'D':1.0,'F':0}
     #create function for get_gpa
-  def get_gpa(self):
-    return self.gpa
+  property
+  def __get_gpa(self):
+    return self.__gpa
   
 #create function to set_gpa,
-  def set_gpa(self,gpa_in):
+  
+  def __set_gpa(self,gpa_in):
 #override any valiue outside of 0 -4 to 0 or 4 respectively
     if gpa_in <= 4 and gpa_in >= 0:
-      self.gpa = gpa_in
+      self.__gpa = gpa_in
     elif gpa_in < 0:
-      self.gpa=0.0
+      self.__gpa=0.0
     else:
-      self.gpa=4.0
+      self.__gpa=4.0
     
 
 
 #create function get_letter
-  def get_letter(self):
+  def __get_letter(self):
     letter_grade=list(self.letters.keys())
     if self.gpa==4.0:
       return letter_grade[0]
@@ -36,31 +38,32 @@ class GPA():
       return letter_grade[4]
       
 #create function set_letter
-  def set_letter(self,letter_in):
+  def __set_letter(self,letter_in):
     self.gpa = self.letters[letter_in]
-
+  
+  letter = property(__get_letter,__set_letter)
+  gpa = property(__get_gpa,__set_gpa)
 def main():
     student = GPA()
 
     print("Initial values:")
-    print("GPA: {:.2f}".format(student.get_gpa()))
-    print("Letter: {}".format(student.get_letter()))
+    print("GPA: {:.2f}".format(student.gpa))
+    print("Letter: {}".format(student.letter))
 
     value = float(input("Enter a new GPA: "))
 
-    student.set_gpa(value)
-
+    student.gpa = value
     print("After setting value:")
-    print("GPA: {:.2f}".format(student.get_gpa()))
-    print("Letter: {}".format(student.get_letter()))
+    print("GPA: {:.2f}".format(student.gpa))
+    print("Letter: {}".format(student.letter))
 
     letter = input("Enter a new letter: ")
 
-    student.set_letter(letter)
+    student.letter = letter
 
     print("After setting letter:")
-    print("GPA: {:.2f}".format(student.get_gpa()))
-    print("Letter: {}".format(student.get_letter()))
+    print("GPA: {:.2f}".format(student.gpa))
+    print("Letter: {}".format(student.letter))
 
 if __name__ == "__main__":
     main()
