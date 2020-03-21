09bbab8443db1308b47943716067af30bbdc402c
Eric Allen <aperic234@gmail.com>
2020-03-21 16:14:07 -0600

    latest update

--- web/nurse_medicine/use_results.js
+++ web/nurse_medicine/use_results.js
@@ -1,22 +1,5 @@
 function search() {
-  const { Pool, Client } = require('pg')
-  const connectionString = 
-  'postgres://kxgzangzhzkpdm:c29f98a332d055f61fc092dbfef16a865ee78b843444b8846dfc3d60fb1c19f4@ec2-184-72-236-3.compute-1.amazonaws.com:5432/dergo389khd0pd'
-  const pool = new Pool({
-    connectionString: connectionString,
-  })
-  pool.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    pool.end()
-  })
-  const client = new Client({
-    connectionString: connectionString,
-  })
-  client.connect()
-  client.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    client.end()
-  })
+  
   // Get the value from the search box
   var searchString = document.getElementById('txtSearch').value;
   console.log('Searching for: ' + searchString);
