6c658af57ab70884d98ce789e09af946d62fe742
Eric Allen <aperic234@gmail.com>
2020-03-21 15:06:08 -0600

    testing environment

--- web/nurse_medicine/use_results.js
+++ web/nurse_medicine/use_results.js
@@ -8,6 +8,7 @@ function search() {
   var url = 'https://www.omdbapi.com/' + params;
   //call fetch with our url...remember that fetch returns a Promise
   //that must be processed with a call to the .then() method.
+  console.log(process.env)
   fetch(url)
     .then(function(response) {
       // fetch also returns a stream as the result...we have to tell it
